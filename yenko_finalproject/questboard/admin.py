f43b28f6026960e6c945d26e6fcce9248647ab5b
Dagny Eran Yenko <dagny.yenko@obf.ateneo.edu>
2021-04-07 12:50:42 +0800

    Set up 'Questboard' model

--- yenko_finalproject/questboard/admin.py
+++ yenko_finalproject/questboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Questboard
+
+
+class QuestboardAdmin(admin.ModelAdmin):
+    model = Questboard
+
+
+admin.site.register(Questboard, QuestboardAdmin)
--- yenko_finalproject/questboard/models.py
+++ yenko_finalproject/questboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Questboard(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    stars = models.IntegerField(default=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1),
+        ]
+    )
+    is_for_everyone = models.BooleanField(default=False)
+    student1 = models.CharField(max_length=100, blank=True)
+    student2 = models.CharField(max_length=100, blank=True)
+    student3 = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return '{}: {} star(s)'.format(self.name, self.stars)
